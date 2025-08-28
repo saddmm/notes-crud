17e01e3825d4ff519c67d57b63c742c3d5a4ca70
saddmm <saddam.abc251@gmail.com>
2025-08-28 13:37:13 +0700

    fix: error di docker compose

--- src/index.ts
+++ src/index.ts
@@ -7,28 +7,29 @@ import swaggerUi from 'swagger-ui-express'
 import 'reflect-metadata'
 import specs from './utils/swagger/config'
 
-const app = express()
-const { PORT } = process.env
-
-app.use(express.json())
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
-
-app.use('/api/users', UserRoute)
-app.use('/api/notes', NoteRoute)
-
-app.get('/api-docs.json', (req, res) => {
-  res.setHeader('Content-Type', 'application/json')
-  res.send(specs)
-})
-
-AppDataSource.initialize()
-  .then(() => {
-    console.log('Database connected')
+const main = async () => {
+  const app = express()
+  const { PORT } = process.env
+  
+  app.use(express.json())
+  
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
+  
+  app.use('/api/users', UserRoute)
+  app.use('/api/notes', NoteRoute)
+  
+  app.get('/api-docs.json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json')
+    res.send(specs)
   })
-  .catch((error: any) => {
-    console.log('error: ', error)
+  
+  await AppDataSource.initialize()
+  console.log('Database connected')
+  app.listen(PORT || 3000, () => {
+    console.log(`Server is running on port ${PORT || 3000}`)
   })
-app.listen(PORT || 3000, () => {
-  console.log(`Server is running on port ${PORT || 3000}`)
+}
+
+main().catch((err) => {
+  console.log('Error: ', err)
 })
